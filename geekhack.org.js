961f2f31280b825d336845d03301b175182dfd65
Steven! Ragnarök <steven@nuclearsandwich.com>
2013-09-03 07:48:58 -0700

    Add tabindexes for keyboard navigation.

--- geekhack.org.js
+++ geekhack.org.js
@@ -7,9 +7,33 @@
 		$(".poster li.threadstarter").parents(".poster").addClass("thread-starter");
 	}
 
-	$(function() {
+	var initKeyboardNavigation = function() {
+		console.log("Hookign up focus");
+
+	}
+
+
+	var selectFirstPost = function() {
+		$("#forumposts .post_wrapper").first().focus();
+	}
+
+	var selectNextPost = function() {
+		return;
+	}
+
+	$(document).ready(function() {
+		$("#forumposts .post_wrapper").each(function(index) {
+			$(this).attr("tabindex", 1000 + index);
+		}).focus(function(event) {
+			$(event.target).toggleClass("js-navigation-focus").toggleClass("navigation-focus");
+		}).blur(function(event) {
+			$(event.target).toggleClass("js-navigation-focus").toggleClass("navigation-focus");
+		});
+
 		identifyThreadStarters();
 		identifyModerators();
+		initKeyboardNavigation();
+		selectFirstPost();
 	})
 })();
 
