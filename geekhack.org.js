94b6c6167f18bec265e8a9805b3af1e399891411
Steven! Ragnarök <steven@nuclearsandwich.com>
2013-09-04 01:07:52 -0700

    Slight refactor in prep for keyable topics.

--- geekhack.org.js
+++ geekhack.org.js
@@ -9,19 +9,39 @@ var identifyThreadStarters = function() {
 };
 
 var initKeyboardNavigation = function() {
-	/* In order to use focus() and blur(), each post needs a tabindex. */
-$("#forumposts .post_wrapper").each(function(index) {
-	$(this).attr("tabindex", 1000 + index);
-}).addClass("js-navigation-target").focus(function(event) {
-var $target = $(event.target);
-var scrollTop = $target.offset().top - topPadding;
+	// mark each navigation container with the js-navigation-container class
+	$("#forumposts").addClass("js-navigation-container");
+	$("#messageindex").addClass("js-navigation-container");
 
-$target.addClass("js-navigation-focus").addClass("navigation-focus")
-$("html").stop().animate({ scrollTop: scrollTop }, 600);
+	// Navigate messageindex rows.
+	$(".js-navigation-container tbody tr").each(function(index) {
+		// In order to use focus() and blur(), each post needs a tabindex.
+		$(this).attr("tabindex", 1000 + index);
+	}).addClass("js-navigation-target").focus(function(event) {
+		var $target = $(event.target);
+		var scrollTop = $target.offset().top - topPadding;
 
-}).blur(function(event) {
-$(event.target).removeClass("js-navigation-focus").removeClass("navigation-focus");
-}).first().focus();
+		$target.addClass("js-navigation-focus").addClass("navigation-focus")
+		$("html").stop().animate({ scrollTop: scrollTop }, 600);
+
+	}).blur(function(event) {
+		$(event.target).removeClass("js-navigation-focus").removeClass("navigation-focus");
+	}).first().focus();
+
+	// Navigate forum posts.
+	$(".js-navigation-container .post_wrapper").each(function(index) {
+		// In order to use focus() and blur(), each post needs a tabindex.
+		$(this).attr("tabindex", 1000 + index);
+	}).addClass("js-navigation-target").focus(function(event) {
+		var $target = $(event.target);
+		var scrollTop = $target.offset().top - topPadding;
+
+		$target.addClass("js-navigation-focus").addClass("navigation-focus")
+		$("html").stop().animate({ scrollTop: scrollTop }, 600);
+
+	}).blur(function(event) {
+		$(event.target).removeClass("js-navigation-focus").removeClass("navigation-focus");
+	}).first().focus();
 };
 
 var navigation = window.Navigation = {
@@ -29,19 +49,21 @@ var navigation = window.Navigation = {
 		"w": 87,
 		"s": 83,
 		"j": 74,
-		"k": 75
+		"k": 75,
+		"enter": 13,
+		"space": 32
 	},
 
 	current: function() {
-		return $("#forumposts .js-navigation-focus")[0]
+		return $(".js-navigation-container .js-navigation-focus")[0]
 	},
 
 	next: function() {
 		var current = this.current();
-		var $posts = $("#forumposts .js-navigation-target");
+		var $posts = $(".js-navigation-container .js-navigation-target");
 		var index = $.inArray(current, $posts);
 		if (typeof(current) === "undefined") {
-			return $("#forumposts .js-navigation-target").first()[0];
+			return $(".js-navigation-container .js-navigation-target").first()[0];
 		} else if (index === $posts.length - 1) {
 			return current;
 		} else {
@@ -51,10 +73,10 @@ var navigation = window.Navigation = {
 
 	previous: function() {
 		var current = this.current();
-		var $posts = $("#forumposts .js-navigation-target");
+		var $posts = $(".js-navigation-container .js-navigation-target");
 		var index = $.inArray(current, $posts);
 		if (typeof(current) === "undefined") {
-			return $("#forumposts .js-navigation-target").first()[0];
+			return $(".js-navigation-container .js-navigation-target").first()[0];
 		} else if (index === 0) {
 			return current;
 		} else {
@@ -64,11 +86,24 @@ var navigation = window.Navigation = {
 };
 
 var enableKeys = function() {
+	var activateInNewTab = function(navigationItem) {
+	}
+
+	var activateHere = function(navigationItem) {
+		$(navigationItem).find(".lastpost a").click();
+	}
+
+	var activatableTargets = $("#messageindex").length > 0;
+
 	$(document).keydown(function(e) {
 		if (e.which === navigation.keys.w || e.which === navigation.keys.k) {
 			navigation.previous().focus();
 		} else if (e.which === navigation.keys.s || e.which === navigation.keys.j) {
 			$(navigation.next()).focus();
+		} else if (activatableTargets && e.which === navigation.keys.space) {
+			activateInNewTab(navigation.current());
+		} else if (activatableTargets && e.which === navigation.keys.enter) {
+			$(navigation.current()).find(".lastpost a").click();
 		}
 	});
 };
