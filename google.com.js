a119e7427a3a5cab4d92e47c55c2168e1e29fc1e
Steven! Ragnarök <steven@nuclearsandwich.com>
2016-06-20 16:38:51 -0400

    Remove sites I don't visit anymore.

--- google.com.js
+++
@@ -1,6 +0,0 @@
-document.body.addEventListener("DOMSubtreeModified", function() {
-	var $fuckerTrimmedContent = $("div[role=button][data-tooltip='Show trimmed content']")
-	if ($fuckerTrimmedContent.length > 0) {
-		$fuckerTrimmedContent.click();
-	}
-});
