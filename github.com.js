38d2ea658f49e535860f0b7c0e81ceaefa205d92
Steven! Ragnarök <steven@nuclearsandwich.com>
2016-08-02 08:52:02 -0700

    Link the commit id to the proper commit url.

--- github.com.js
+++ github.com.js
@@ -1,1 +1,9 @@
-$('.reorderable-task-lists').removeClass('reorderable-task-lists js-reorderable-task-lists');
+
+var match_data = window.location.pathname.match(/^(\/[-_\w]+\/[-_\w]+\/)pull\/\d+\/commits/);
+if (match_data) {
+	var $commit = $('.commit-meta .sha');
+	var commit_sha = $commit.text();
+	var anchor_tag = '<a href="' + match_data[1] + 'commit/' + commit_sha + '">' + commit_sha + '</a>';
+	$commit.html(anchor_tag);
+}
+
